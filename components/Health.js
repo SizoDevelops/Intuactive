8f6828e3aa17f482b7ffd8d9df92304c03878bb8
Osmond Matsela <osmondmatsela@gmail.com>
2024-10-24 03:29:37 +0200

    Overview

--- components/Health.js
+++ components/Health.js
@@ -23,8 +23,8 @@ export default function Health() {
     }, []),
   );
 
-  const lineData = [
-    {value: 60, dataPointText: '0'},
+  const BloodPressure = [
+    {value: 60, dataPointText: '0' },
     {value: 20, dataPointText: '10'},
     {value: 40, dataPointText: '8'},
     {value: 68, dataPointText: '58'},
@@ -38,7 +38,7 @@ export default function Health() {
     {value: 58, dataPointText: '98'},
   ];
 
-  const lineData2 = [
+  const HeartRate = [
     {value: 0, dataPointText: '0'},
     {value: 30, dataPointText: '20'},
     {value: 48, dataPointText: '18'},
@@ -53,6 +53,35 @@ export default function Health() {
     {value: 25, dataPointText: '85'},
   ];
 
+  const SleepPattern = [
+    {value: 0, dataPointText: '0'},
+    {value: 70, dataPointText: '20'},
+    {value: 68, dataPointText: '18'},
+    {value: 10, dataPointText: '40'},
+    {value: 16, dataPointText: '36'},
+    {value: 20, dataPointText: '56'},
+    {value: 50, dataPointText: '78'},
+    {value: 34, dataPointText: '74'},
+    {value: 58, dataPointText: '98'},
+    {value: 10, dataPointText: '60'},
+    {value: 44, dataPointText: '54'},
+    {value: 25, dataPointText: '85'},
+  ];
+
+  const BloodOxygenSaturation = [
+    {value: 0, dataPointText: '0'},
+    {value: 50, dataPointText: '20'},
+    {value: 68, dataPointText: '18'},
+    {value: 10, dataPointText: '40'},
+    {value: 76, dataPointText: '36'},
+    {value: 40, dataPointText: '56'},
+    {value: 70, dataPointText: '78'},
+    {value: 24, dataPointText: '74'},
+    {value: 18, dataPointText: '98'},
+    {value: 60, dataPointText: '60'},
+    {value: 44, dataPointText: '54'},
+    {value: 25, dataPointText: '85'},
+  ];
 
 
   return (
@@ -62,30 +91,72 @@ export default function Health() {
         <Text style={styles.Text}>
           Medical Portfolio
         </Text>
-        <Text style={[styles.Text, {fontSize: 48, fontWeight: "900", marginTop: 7, marginBottom: 30}]}>
+        <Text style={[styles.Text, {fontSize: 40, fontWeight: "900", marginTop: 2, marginBottom: 30}]}>
           Overview
         </Text>
       </View>
 
       <View style={styles.Graph}>
       <LineChart
-          data={lineData}
-          data2={lineData2}
-          height={250}
+          data={BloodPressure}
+          data2={HeartRate}
+          data3={SleepPattern}
+          data4={BloodOxygenSaturation}
+          height={200}
           showVerticalLines = {false}
           spacing={44}
           initialSpacing={0}
-          color1="skyblue"
-          color2="orange"
-          textColor1="green"
+          color1= {Colors.RED}
+          color2= {Colors.GREEN}
+          color3= {Colors.ORANGE}
+          color4= {Colors.AQUA}
           hideDataPoints
           hideRules
-          
+          yAxisColor={Colors.TXT}
+          xAxisColor={Colors.TXT}
           textShiftY={-2}
           textShiftX={-5}
           textFontSize={13}
           curved
       />
+      </View>
+      <View>
+          {/* <View>
+            <Text style={styles.Text}>Legend</Text>
+          </View> */}
+          <View style = {styles.Container}>
+            {/* <View style={styles.Colmn}> */}
+            <View style={styles.Colmn}>    
+              <Text style={styles.text}>Blood pressure |</Text>
+              <View style={[styles.Block, {backgroundColor:Colors.RED}]}/>
+            </View>
+            <View style={styles.Colmn}>
+              <Text style={styles.text}>Heart rate |</Text>
+              <View style={[styles.Block, {backgroundColor:Colors.GREEN}]}/>
+            </View>
+            <View style={styles.Colmn}>
+              <Text style={styles.text}>Sleep Patterns |</Text>
+              <View style={[styles.Block, {backgroundColor:Colors.ORANGE}]}/>  
+            </View>
+            <View style={styles.Colmn}>
+              <Text style={styles.text}>Blood Oxygen Saturation |</Text>
+              <View style={[styles.Block, {backgroundColor:Colors.AQUA}]}/>
+            </View>
+          </View>  
+            {/* <View style={[styles.Block, {backgroundColor:Colors.PURPLE}]}/>
+            <Text style={styles.text}>Calorie Burn</Text>
+          </View> */}
+          
+          
+          
+          
+          
+            
+            
+          
+          
+
+          
       </View>
 
 
@@ -104,6 +175,29 @@ const styles = StyleSheet.create({
       paddingHorizontal: 20
     },
     Graph: {
-      marginTop: 100
+      marginTop: 80
+    },
+    Container:{
+      flexDirection: 'row',
+      gap: 5,
+      paddingHorizontal: 10,
+      justifyContent: 'space-between'
+    },
+    Colmn:{
+      flexDirection: 'colmn',
+      // paddingHorizontal: 10,
+      // gap:45
+    },
+    text:{
+      color: Colors.TXTALT,
+      fontSize: 10
+    },
+    Block:{
+      height: 5,
+      width: 20,
+      marginBottom: 8,
+      marginTop: 2,
+      borderRadius: 25
     }
+
   })
--- shared/Colors.js
+++ shared/Colors.js
@@ -3,5 +3,10 @@ export const Colors = {
     BGALT: "#FFFFFF",
     TXT: "#FFFFFF",
     TXTALT: "#121127",
-    TXTLT: "#9D9D9D"
+    TXTLT: "#9D9D9D",
+    RED: "#FF0606",
+    GREEN: "#06FF34",
+    ORANGE: "#FFA806",
+    PURPLE: "#8B06FF",
+    AQUA: "#06FFFF",
 }
