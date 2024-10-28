80f8bd2da035fbaeac28b96cc5a40666905aaa66
SizoDevelops <fierceincii@gmail.com>
2024-10-28 20:24:07 +0200

    This

--- shared/Gemini.js
+++ shared/Gemini.js
@@ -1,38 +1,10 @@
 
-import Config from "../config";
-
-
-// export const  TalkToAI = async (prompt) =>  {
-//   try {
-//     const genAI = new GoogleGenerativeAI(Config.GOOGLE_MAPS_API_KEY);
-//     if (!genAI) {
-//       throw new Error("GoogleGenerativeAI instance is null");
-//     }
-//     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-//     if (!model) {
-//         return "I'm unable to respond right now.";
-//     }
-
-
-//     const result = await model.generateContent(prompt);
-//     if (!result) {
-//       return "I'm unable to respond right now.";
-//     }
-
-//     return result.response.text();
-
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
+import {GOOGLE_MAPS_API_KEY} from '@env'
 const {
   GoogleGenerativeAI,
-  HarmCategory,
-  HarmBlockThreshold,
 } = require("@google/generative-ai");
 
-const apiKey = Config.GOOGLE_MAPS_API_KEY
+const apiKey = GOOGLE_MAPS_API_KEY
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
